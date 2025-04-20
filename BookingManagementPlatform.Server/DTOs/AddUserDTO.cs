490b3ef4c0f56dbe800a28ba50326fab5a603019
halawardeh <halawardeh12102001@gmail.com>
2025-04-20 17:31:09 +0300

    Add booking, Cancelation and history by Hala

---
+++ BookingManagementPlatform.Server/DTOs/AddUserDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace BookingManagementPlatform.Server.DTOs
+{
+    //==================== DTO added by Hala ====================================================
+
+    public class AddUserDTO
+    {
+        public string? Name { get; set; }
+
+        public string? Email { get; set; }
+
+    }
+}
--- BookingManagementPlatform.Server/IDataSerivcee/IUserServicee.cs
+++ BookingManagementPlatform.Server/IDataSerivcee/IUserServicee.cs
@@ -1,4 +1,5 @@
 ﻿using BookingManagementPlatform.Server.DTOs;
+using BookingManagementPlatform.Server.Models;
 
 namespace BookingManagementPlatform.Server.IDataSerivcee
 {
@@ -10,6 +11,29 @@ namespace BookingManagementPlatform.Server.IDataSerivcee
         Task<string?> GoogleLoginAsync(string idToken);
         public bool VerifyResetCode(string email, string inputCode);
         Task<bool> ResetPasswordAsync(string email, string newPassword);
+
+
+
+        //==================== start of hala services ====================================================
+
+        public List<User> getAllusers();
+
+        public List<Booking> getAllBooking();
+        public List<RoomsCategory> GetAllRoomsCategories();
+        public List<Room> GetAllRooms();
+        Booking AddBooking(int userId, BookingByID bookingDto);
+
+        public List<Booking> getallbookings(int UserId);
+        public List<Room> getRoomByID(int UserId);
+
+        bool IsRoomAvailable(int roomId, DateTime startDateTime, DateTime endDateTime);
+
+        public Booking getBookById(int BookID);
+        void CancelBooking(int bookingId);
+
+
+        //==================== End of hala services ====================================================
+
 
     }
 }
